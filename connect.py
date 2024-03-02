b4517e5ebd36d30389d695fbbd1b7e7ea7f5825b
Grigory-Yunusov <grigoriyzawr@gmail.com>
2024-03-02 16:00:11 +0300

    dont save

--- connect.py
+++ connect.py
@@ -1,15 +1,17 @@
 from mongoengine import connect
 import configparser
 
+def connect_to_db():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+    mongo_user = config.get('DB', 'user')
+    mongodb_pass = config.get('DB', 'pass')
+    db_name = config.get('DB', 'db_name')
+    domain = config.get('DB', 'domain')
 
-mongo_user = config.get('DB', 'user')
-mongodb_pass = config.get('DB', 'pass')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
+    # Підключення до кластера на AtlasDB за допомогою рядка підключення
+    connect(host=f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority", ssl=True)
 
-# connect to cluster on AtlasDB with connection string
-
-connect(host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
+# Виклик функції для підключення
+# connect_to_db()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mongoengine import StringField, Document, ObjectIdField, ReferenceField, Li
 import uuid
 
 class Author(Document):
-    id = StringField(primary_key=True, default=str(uuid.uuid4()), max_length=36)
+    # id = StringField(primary_key=True, default=str(uuid.uuid4()), max_length=36)
     fullname = StringField(max_length=50)
     born_date = StringField(max_length=50)
     born_location = StringField(max_length=50)
@@ -11,7 +11,7 @@ class Author(Document):
 
 
 class Quote(Document):
-    id = StringField(primary_key=True, default=str(uuid.uuid4()), max_length=36)
+    # id = StringField(primary_key=True, default=str(uuid.uuid4()), max_length=36)
     tags = ListField(StringField(max_length=50))
     quote = StringField(max_length=1050)
 
